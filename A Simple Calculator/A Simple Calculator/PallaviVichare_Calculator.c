4b2a01f89bfcc6abd05ec215db3db52f8a943808
Pallavi Vichare <vivspal@Pallavis-MacBook-Pro.local>
2014-10-05 16:41:42 -0700

    Created an empty project (command line tool) in Xcode for the mid-term assignment: A Simple Calculator.

---
+++ A Simple Calculator/A Simple Calculator/PallaviVichare_Calculator.c
@@ -0,0 +1,15 @@
+//
+//  PallaviVichare_Calculator.c
+//  A Simple Calculator
+//
+//  Created by Pallavi Vichare on 10/5/14.
+//  Copyright (c) 2014 Pallavi Vichare. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
